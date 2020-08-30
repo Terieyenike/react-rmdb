ec741679fb2526ac7d84145bed667c242be59a22
Teri Eyenike <teyenike1@gmail.com>
2020-08-30 18:28:42 +0100

    changed the directory to live site

---
+++ build/precache-manifest.c44190eb05936ec72dded86ecc56ca53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8979c8ef2b3c2ab1a1ebdb424124c7e",
+    "url": "/react-rmdb/index.html"
+  },
+  {
+    "revision": "e79b35eb306fce6fdaf9",
+    "url": "/react-rmdb/static/css/main.072282aa.chunk.css"
+  },
+  {
+    "revision": "9ad71dab50078806380c",
+    "url": "/react-rmdb/static/js/2.15fc8adc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-rmdb/static/js/2.15fc8adc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e79b35eb306fce6fdaf9",
+    "url": "/react-rmdb/static/js/main.c36633f6.chunk.js"
+  },
+  {
+    "revision": "9ade43deb76b26e9a367",
+    "url": "/react-rmdb/static/js/runtime-main.8934166f.js"
+  },
+  {
+    "revision": "08494abf4210d1ea8b163791db70773c",
+    "url": "/react-rmdb/static/media/reactMovie_logo.08494abf.png"
+  }
+]);
